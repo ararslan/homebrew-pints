16242f11e0bda46092443f9dff483d2e51e6f0a2
Alex Arslan <ararslan@comcast.net>
2016-07-01 14:01:44 -0700

    Added wsedit prerelease

---
+++ wsedit.rb
@@ -0,0 +1,20 @@
+class Wsedit < Formula
+  desc "A simple terminal source code editor"
+  homepage "https://github.com/SirBoonami/wsedit"
+  url "https://github.com/SirBoonami/wsedit/archive/v0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "f5a557e5c120b9879575fc2c98031ece84fb4bb8e2e47da72d4ddbab6ad1cbe5"
+  head "https://github.com/SirBoonami/wsedit.git"
+
+  depends_on "haskell-stack" => :build
+
+  def install
+    system "stack", "setup"
+    system "stack", "install"
+    # TODO: wsedit is installed in ~/.local/bin -- should go elsewhere
+  end
+
+  test do
+    # TODO
+  end
+end
