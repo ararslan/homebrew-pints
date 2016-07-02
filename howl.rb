2a053d0ad8f4fc54ad374c79a3ec6a71d74bdf88
Alex Arslan <ararslan@comcast.net>
2016-07-01 23:15:41 -0700

    Caveats

--- howl.rb
+++ howl.rb
@@ -6,7 +6,6 @@ class Howl < Formula
   sha256 "25075b12ba0138afa1cb694c52dd756b35a5350fe3b30132fbcacd137db3adb3"
   head "https://github.com/howl-editor/howl.git"
 
-  depends_on :x11 => :build
   depends_on "gtk+3"
   depends_on "wget" => :build if build.head?
 
@@ -15,5 +14,14 @@ class Howl < Formula
       system "make", "PREFIX=#{prefix}", "install"
       bin.install "howl"
     end
+  end
+
+  def caveats
+    <<-EOS.undent
+      Howl is primarily targeting Linux at the moment, so there may be issues on OS X that
+      haven't yet made it on the developers' radar. Homebrew installation issues can be
+      reported at https://github.com/ararslan/homebrew-pints but all other Howl issues
+      should be reported at https://github.com/howl-editor/howl.
+    EOS
   end
 end
