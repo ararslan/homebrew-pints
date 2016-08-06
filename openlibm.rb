a9a28ececa80218bd1b176e7b4d0e684a4c241e5
Alex Arslan <ararslan@comcast.net>
2016-08-06 12:52:20 -0700

    OpenLibm v0.5.2

--- openlibm.rb
+++ openlibm.rb
@@ -1,9 +1,9 @@
 class Openlibm < Formula
   desc "High quality, system independent, portable, open source libm implementation"
   homepage "http://www.openlibm.org"
-  url "https://github.com/JuliaLang/openlibm/archive/v0.5.1.tar.gz"
-  version "0.5.1"
-  sha256 "782a77497071cc450af176bbfebb34834334f6a17de842fe75cd407bbee89f85"
+  url "https://github.com/JuliaLang/openlibm/archive/v0.5.2.tar.gz"
+  version "0.5.2"
+  sha256 "f394c90426356c8911333def8cee94971492a94ca94578e61ad857913c80122c"
   head "https://github.com/JuliaLang/openlibm.git"
 
   def install
@@ -13,7 +13,7 @@ class Openlibm < Formula
 
     system "make", "install", "prefix=."
 
-    lib.install Dir["lib/*"].reject {|f| File.directory? f }
+    lib.install Dir["lib/*"].reject { |f| File.directory? f }
     (lib/"pkgconfig").install Dir["lib/pkgconfig/*"]
     (include/"openlibm").install Dir["include/openlibm/*"]
   end
