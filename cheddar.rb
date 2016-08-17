1b99f8bf734e091497169a8c5136ec12334e64d9
Alex Arslan <ararslan@comcast.net>
2016-08-16 18:57:55 -0700

    Forgot the SHA

--- cheddar.rb
+++ cheddar.rb
@@ -2,6 +2,7 @@ class Cheddar < Formula
   desc "The Cheddar programming language"
   homepage "http://cheddar.vihan.org"
   url "https://github.com/cheddar-lang/Cheddar/archive/v1.0.0-beta.45.tar.gz"
+  sha256 "6865314e2d9e37898c3e047146d032841b12e90da159681b18f2304e09062c53"
   version "1.0.0-beta.45"
   head "https://github.com/cheddar-lang/Cheddar.git"
 
