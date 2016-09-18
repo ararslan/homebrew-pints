42640e2f19ed1a58ad63a0d54badf878a8e54ce4
Alex Arslan <ararslan@comcast.net>
2016-09-18 11:20:25 -0700

    wsedit 1.1.0 :tada:

--- wsedit.rb
+++ wsedit.rb
@@ -1,18 +1,19 @@
 class Wsedit < Formula
   desc "A simple terminal source code editor"
   homepage "https://github.com/SirBoonami/wsedit"
-  url "https://github.com/SirBoonami/wsedit/archive/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "db8c8ce5588fc76c2d7ef5a9a9e264d7a3e2fdd37d4ef21be7cc98ccad5b4f79"
+  url "https://github.com/SirBoonami/wsedit/archive/v1.1.0.tar.gz"
+  version "1.1.0"
+  sha256 "e9812a8bf8295a32945403e4f22cad6c1e92a8f85ee221a6b404f279b2ede704"
   head "https://github.com/SirBoonami/wsedit.git"
 
   depends_on "haskell-stack" => :build
 
-  bottle do
-    root_url "https://dl.bintray.com/ararslan/wsedit/"
-    cellar :any_skip_relocation
-    sha256 "2915443e111959ca5d7bd5f9e770293522d9ee129fedeb81bb97bf36c5d7751b" => :yosemite
-  end
+  # FIXME: This corresponds to v1.0.0. Need to update for 1.1.0.
+  # bottle do
+  #   root_url "https://dl.bintray.com/ararslan/wsedit/"
+  #   cellar :any_skip_relocation
+  #   sha256 "2915443e111959ca5d7bd5f9e770293522d9ee129fedeb81bb97bf36c5d7751b" => :yosemite
+  # end
 
   def install
     bin.mkpath
