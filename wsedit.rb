02ae0abdb66d99422e3d4a7d6558a0ff0146f36c
Alex Arslan <ararslan@comcast.net>
2018-01-30 17:29:43 -0800

    Updates for brew audit --new-formula

--- wsedit.rb
+++ wsedit.rb
@@ -1,19 +1,18 @@
 class Wsedit < Formula
-  desc "A simple terminal source code editor"
+  desc "Simple terminal source code editor"
   homepage "https://github.com/LadyBoonami/wsedit"
   url "https://github.com/LadyBoonami/wsedit/archive/v1.2.2.tar.gz"
-  version "1.2.2"
   sha256 "3fc1e291adb01e1e37746cc6d2232f2d389c0e92ff6dfcca134d10385033ee06"
   head "https://github.com/LadyBoonami/wsedit.git"
 
-  depends_on "haskell-stack" => :build
-
   option "without-language-files", "Do not install language highlighting definition files"
+
+  depends_on "haskell-stack" => :build
 
   def install
     bin.mkpath
     system "stack", "setup"
-    system "stack", "install", "--local-bin-path", "#{prefix}/bin"
+    system "stack", "install", "--local-bin-path", bin
     unless build.without? "language-files"
       langdir = etc/"wsedit/lang/default"
       langdir.mkpath
