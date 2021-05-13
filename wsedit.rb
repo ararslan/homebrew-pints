c3a273b76a5daae9e5201ef69ed5bb560ed72c9d
Alex Arslan <ararslan@comcast.net>
2021-05-13 10:43:59 -0700

    Bump WSEdit to v1.2.4
    Now under BSD-3 license

--- wsedit.rb
+++ wsedit.rb
@@ -1,8 +1,8 @@
 class Wsedit < Formula
   desc "Simple terminal source code editor"
   homepage "https://github.com/LadyBoonami/wsedit"
-  url "https://github.com/LadyBoonami/wsedit/archive/v1.2.2.tar.gz"
-  sha256 "3fc1e291adb01e1e37746cc6d2232f2d389c0e92ff6dfcca134d10385033ee06"
+  url "https://github.com/LadyBoonami/wsedit/archive/v1.2.4.tar.gz"
+  sha256 "b1847007cfd99adc16c7664a9970896f8738c57ebd18974c3d026060680aa44c"
   head "https://github.com/LadyBoonami/wsedit.git"
 
   option "without-language-files", "Do not install language highlighting definition files"
