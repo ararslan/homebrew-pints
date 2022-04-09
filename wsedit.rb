daf02a7e6404faa0b0b681647e12b772f625bb04
Alex Arslan <ararslan@comcast.net>
2022-04-08 22:11:01 -0700

    Update wsedit to v1.2.5

--- wsedit.rb
+++ wsedit.rb
@@ -1,8 +1,8 @@
 class Wsedit < Formula
   desc "Simple terminal source code editor"
   homepage "https://github.com/LadyBoonami/wsedit"
-  url "https://github.com/LadyBoonami/wsedit/archive/v1.2.4.tar.gz"
-  sha256 "b1847007cfd99adc16c7664a9970896f8738c57ebd18974c3d026060680aa44c"
+  url "https://github.com/LadyBoonami/wsedit/archive/v1.2.5.tar.gz"
+  sha256 "6047f086cc11c625c9fc142e611b0280ec703dbb8ab8690494429c1f4105ae3b"
   head "https://github.com/LadyBoonami/wsedit.git"
 
   option "without-language-files", "Do not install language highlighting definition files"
